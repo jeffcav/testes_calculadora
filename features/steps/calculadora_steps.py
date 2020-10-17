d2e7c323e667ec7de29eefb07446116e712b1ad1
Jefferson Cavalcante <jefferson.cavalcante@hpe.com>
2020-10-17 04:52:24 -0300

    Melhorando legibilidade dos steps

--- features/steps/calculadora_steps.py
+++ features/steps/calculadora_steps.py
@@ -2,18 +2,18 @@
 from behave import given, when, then, step
 from calculadora import Calculadora, CalculadoraPrecisao
 
-@given('numbers {valueA:f} and {valueB:f}')
-def step_impl(context, valueA, valueB):
-    context.valA = valueA    
-    context.valB = valueB
+@given('numbers {a:f} and {b:f}')
+def step_impl(context, a, b):
+    context.a = a    
+    context.b = b
     pass
 
 @then('sum should be {result:f}')
-def step_impl(context, result):  # -- NOTE: number is converted into integer
+def step_impl(context, result):
     c = Calculadora()
-    assert c.soma(context.valA, context.valB) == result
+    assert c.soma(context.a, context.b) == result
 
 @then('sum with precision {precision:d} should be {result:f}')
 def step_impl(context, precision, result):
     c = CalculadoraPrecisao(precision)
-    assert c.soma(context.valA, context.valB) == result
+    assert c.soma(context.a, context.b) == result
